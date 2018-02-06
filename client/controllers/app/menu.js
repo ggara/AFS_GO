e9257db20b7c6051959c46ac1fc5fa0da6e2a2e5
Guillermo Garate Amaral <guillermo.garate@flex.com>
2018-02-06 09:29:52 -0800

    [Studio] Modified screen "menu"

--- client/controllers/app/menu.js
+++ client/controllers/app/menu.js
@@ -3,3 +3,22 @@ function app_menu($scope, app) {
     'use strict';
     app.init($scope);
 }
+
+$scope.alert = function () {
+        $ionicPopup.show({
+            title: 'About',
+            subTitle: 'This is an alert box, click OK button',
+            scope: $scope,
+            buttons: [{
+                    text: '<b>Ok</b>',
+                    type: 'button-positive',
+                    onTap: function (e) {
+                        if (window.mocks) {
+                            $scope.app.data.alertConfirmationMessage = 'This is a warning message!';
+                        } else {
+                            app.call('popup.alert');
+                        }
+                    }
+                }]
+        });
+    };
