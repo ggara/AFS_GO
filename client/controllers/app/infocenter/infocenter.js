1224921454ee07bffc9810099dfd3795ca71cdda
Guillermo Garate Amaral <guillermo.garate@flex.com>
2018-02-08 07:33:13 -0800

    [Studio] Modified screen "infocenter"

--- client/controllers/app/infocenter/infocenter.js
+++ client/controllers/app/infocenter/infocenter.js
@@ -2,4 +2,24 @@ angular.module('app').controller('app_infocenter_infocenter', app_infocenter_inf
 function app_infocenter_infocenter($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.data.tabs = {
+                      "tabs": [
+                        {
+                          "name": "Tab One",
+                          "icon": "ion-ios-information",
+                          "selected": true
+                        },
+                        {
+                          "name": "Tab Two",
+                          "icon": "ion-ios-bell",
+                          "selected": false
+                        },
+                        {
+                          "name": "Tab Three",
+                          "icon": "ion-help-buoy",
+                          "selected": false
+                        }
+                      ]
+                    };
 }
