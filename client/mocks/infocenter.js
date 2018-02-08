fd33735e1ad29b4c0c4160877917ede6d35365e0
Guillermo Garate Amaral <guillermo.garate@flex.com>
2018-02-08 09:46:20 -0800

    [Studio] Update mock actions for "infocenter", Update mock actions for "about", Modified screen "infocenter"

--- client/mocks/infocenter.js
+++ client/mocks/infocenter.js
@@ -2,4 +2,10 @@
 window.mocks = window.mocks || {};
 window.mocks.infocenter = window.mocks.infocenter || {};
 window.mocks.infocenter.tabs_action = function ($scope, params, app) {
+    function Carton() {
+        app.go('app.infocenter.infocenter');
+    }
+    function Batch() {
+        app.go('app.infocenter.infocenter');
+    }
 };
