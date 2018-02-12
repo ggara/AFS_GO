10117b3dc7fe7a7ed0616b05677b2327405278f3
Guillermo Garate <guillermo.garate@flex.com>
2018-02-12 16:40:35 -0600

    Visual Sutio Code

---
+++ server/api/infocenter.js
@@ -0,0 +1,24 @@
+var config = require('../config');
+var browser =  require('powwow-server-common').browser;
+var Client = require('node-rest-client'.Client);
+
+function getBatch(page, batchNumber, res) {
+	var url = "ldap://" + config.adURL;
+
+	// direct way 
+    var client = new Client();
+    
+    var args = {
+        parameters: { BatchNo: batchNumber }, // this is serialized as URL parameters 
+    };    
+    
+    client.get("http://localhost/Xtend/AFS/api/InfoCenter/GetBatch", args,
+        function (data, response) {
+            // parsed response body as js object 
+            $scope.console.log(data);
+            // raw response 
+            $scope.console.log(response);
+        });
+}
+
+exports.infocenter = getBatch;
