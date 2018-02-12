0ea83666d3db1e5cd7c86daee28ff9fc873836e6
Guillermo Garate <guillermo.garate@flex.com>
2018-02-12 16:47:12 -0600

    Visual Studio Code

--- server/api/infocenter.js
+++ server/api/infocenter.js
@@ -1,24 +1,27 @@
 var config = require('../config');
 var browser =  require('powwow-server-common').browser;
-var Client = require('node-rest-client'.Client);
+// var Client = require('node-rest-client'.Client);
 
 function getBatch(page, batchNumber, res) {
-	var url = "ldap://" + config.adURL;
 
-	// direct way 
-    var client = new Client();
+    $scope.console.log("Entro");
+
+	// var url = "ldap://" + config.adURL;
+
+	// // direct way 
+    // var client = new Client();
     
-    var args = {
-        parameters: { BatchNo: batchNumber }, // this is serialized as URL parameters 
-    };    
+    // var args = {
+    //     parameters: { BatchNo: batchNumber }, // this is serialized as URL parameters 
+    // };    
     
-    client.get("http://localhost/Xtend/AFS/api/InfoCenter/GetBatch", args,
-        function (data, response) {
-            // parsed response body as js object 
-            $scope.console.log(data);
-            // raw response 
-            $scope.console.log(response);
-        });
+    // client.get("http://localhost/Xtend/AFS/api/InfoCenter/GetBatch", args,
+    //     function (data, response) {
+    //         // parsed response body as js object 
+    //         $scope.console.log(data);
+    //         // raw response 
+    //         $scope.console.log(response);
+    //     });
 }
 
 exports.infocenter = getBatch;
