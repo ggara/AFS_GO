535773d02264681bce0e9bf1d59d68c0a24f2044
Guillermo Garate <guillermo.garate@flex.com>
2018-02-13 11:47:25 -0600

    Visual Stuido Code

--- server/api/infocenter.js
+++ server/api/infocenter.js
@@ -1,27 +1,31 @@
 var config = require('../config');
 var browser =  require('powwow-server-common').browser;
-// var Client = require('node-rest-client'.Client);
+var Client = require('node-rest-client').Client;
 
 function getBatch(page, batchNumber, res) {
 
-    $scope.console.log("Entro");
-
 	// var url = "ldap://" + config.adURL;
 
-	// // direct way 
-    // var client = new Client();
+	// direct way 
+    var client = new Client();
     
-    // var args = {
-    //     parameters: { BatchNo: batchNumber }, // this is serialized as URL parameters 
-    // };    
+    var args = {
+        parameters: { BatchNo: batchNumber }, // this is serialized as URL parameters 
+    };    
     
-    // client.get("http://localhost/Xtend/AFS/api/InfoCenter/GetBatch", args,
-    //     function (data, response) {
-    //         // parsed response body as js object 
-    //         $scope.console.log(data);
-    //         // raw response 
-    //         $scope.console.log(response);
-    //     });
+    client.get("http://localhost/Xtend/AFS/api/InfoCenter/GetBatch", args,
+        function (data, response) {
+            // parsed response body as js object 
+            //console.log(data);
+            // raw response 
+            //console.log(response);
+            page.data(function (data) {              
+                data.batch.batchNumber = batchNumber;
+            }).screen('infocenter'); 
+
+        }).on('error', function (err) {
+            console.log('something went wrong on the request', err.request.options);
+        });
 }
 
-exports.infocenter = getBatch;
+exports.getBatch = getBatch;
