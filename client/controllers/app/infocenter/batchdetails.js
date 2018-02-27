fe1fb71b36023475b48298d8b47d079704ac2619
Guillermo Garate Amaral <guillermo.garate@flex.com>
2018-02-27 07:46:32 -0800

    [Studio] Delete screen "app.infocenter.batchdetails"

--- client/controllers/app/infocenter/batchdetails.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').controller('app_infocenter_batchdetails', app_infocenter_batchdetails);
-function app_infocenter_batchdetails($scope, app) {
-    'use strict';
-    app.init($scope);
-}
