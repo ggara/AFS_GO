90e3b0000760f8f7dc0b21dddca5b1bda2d31b99
Guillermo Garate Amaral <guillermo.garate@flex.com>
2018-02-28 14:37:37 -0800

    [Studio] Modified screen "Home"

--- client/controllers/app/home.js
+++ client/controllers/app/home.js
@@ -2,6 +2,4 @@ angular.module('app').controller('app_home', app_home);
 function app_home($scope, app) {
     'use strict';
     app.init($scope);
-    
-    var username = $localStorage.username;
 }
