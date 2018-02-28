cfc7a9b5ddc318f4c0c77524d0d3711257ed0d3b
Guillermo Garate <guillermo.garate@flex.com>
2018-02-28 11:35:35 -0600

    Visual Studio Code

--- server/api/infocenter.js
+++ server/api/infocenter.js
@@ -1,12 +1,10 @@
 var config = require('../config');
 var browser =  require('powwow-server-common').browser;
 var request = require('request')
-
+var userName = 'gdlggara@Americas';
+var password = 'Jueves.78';
 
 function getBatch(page, batchNumber, res) {
-
-    var userName = 'gdlggara@Americas';
-    var password = 'Jueves.78';
 
     var options = {
       parameters: { BatchNo: batchNumber },
@@ -28,10 +26,43 @@ function getBatch(page, batchNumber, res) {
 
         page.data(function (data) {
           data.batch = batch;
+          if(!body){
+            data.errorMessage = "Batch not found !";
+          }
         }).screen('Infocenter.batchDetails');
 
       })
     
 }
 
+function getCarton(page, cartonNumber, res) {
+
+  var options = {
+    url: 'http://'+ config.xtendServer +'/Xtend/AFS/api/InfoCenter/GetCarton?CartonNo=' + cartonNumber,
+    auth: {
+        user: userName,
+        password: password
+      } 
+    };
+
+  request(options, function (err, res, body) {
+      if (err) {
+        console.dir(err)
+        return
+      }
+      
+      var carton = JSON.parse("[" + body + "]");
+
+      page.data(function (data) {
+        data.carton = carton;
+        if(!body){
+          data.errorMessage = "Carton not found !";
+        }
+      }).screen('Infocenter.cartonDetails');
+
+    })
+  
+}
+
 exports.getBatch = getBatch;
+exports.getCarton = getCarton;
