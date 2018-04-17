3e6c0a32c5789ec9e52bfbf9bc99051b8f1171e4
Guillermo Garate Amaral <guillermo.garate@flex.com>
2018-04-17 10:32:55 -0700

    [Studio] Modified screen "Login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -1,5 +1,5 @@
 angular.module('app').controller('app_login', app_login);
-function app_login($scope, app, $q, $localStorage) {
+function app_login($scope, app, $q) {
     'use strict';
     app.init($scope);
     /*if (!$scope.data) {
