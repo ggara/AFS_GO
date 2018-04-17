fd8b001b627ee00c494c3d82d391479116278d68
Guillermo Garate Amaral <guillermo.garate@flex.com>
2018-04-17 09:49:06 -0700

    [Studio] Modified screen "Login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -1,15 +1,15 @@
 angular.module('app').controller('app_login', app_login);
-function app_login($scope, app, $q) {
+function app_login($scope, app, $q, $localStorage) {
     'use strict';
     app.init($scope);
     /*if (!$scope.data) {
         $scope.data = {};
     }*/
     
-    setTimeout(function () {
+    /*setTimeout(function () {
         $scope.data.username = $localStorage.username;
         $scope.data.domain = $localStorage.domain;
-    });
+    });*/
     
     //$rootScope.currentServer = $localStorage.currentServer;
     
@@ -42,7 +42,7 @@ function app_login($scope, app, $q) {
             $scope.data.errorMessage = "Invalid Domain";
         }
         else{
-            //$localStorage.username = $scope.data.username;
+            $localStorage.username = $scope.data.username;
             var user = {
                 userName: $scope.data.username,
                 password: $scope.data.password,
